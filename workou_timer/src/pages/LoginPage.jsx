33d42748c6a2dd6ee71883c6a6f85780b3835330
AJ Seidler <ajseidler0526@gmail.com>
2024-02-02 15:50:31 -0600

    updated login css

--- workou_timer/src/pages/LoginPage.jsx
+++ workou_timer/src/pages/LoginPage.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom"
 export default function LoginPage() {
   const {user, setUser} = useUserContext()
   const navigate = useNavigate();
+  const [hidden, setHidden] = useState(false)
 
   // Sign in with Google button
   function SignInButton() {
@@ -24,9 +25,9 @@ export default function LoginPage() {
     };  
 
     return (
-      <button className="btn-google" onClick={signInWithGoogle}>
-        <img src={'/googlelogo.png'} /> Sign in with Google
-      </button>
+      <a className="icon" onClick={signInWithGoogle}>
+        <i className="fa-brands fa-google-plus-g"></i>
+      </a>
     );
   }
   
@@ -97,10 +98,56 @@ export default function LoginPage() {
   // 2. user signed in, but missing username <UsernameForm />
   // 3. user signed in, has username <SignOutButton />
   return (
-    <main>
-      {/* <Metatags title="Enter" description="Sign up for this amazing app!" /> */}
-      {user ? <SignOutButton /> : <SignInButton />}
-    </main>
+    <div className={`cont ${hidden && 'active'}`} id='cont'>
+      <div className='form-cont sign-up'>
+        <form>
+          <h1>Create Account</h1>
+          <div className="social-icons">
+            <SignInButton />
+            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+          </div>
+          <span>or use your email for registeration</span>
+          <input type="text" placeholder="Name"/>
+          <input type="email" placeholder="Email"/>
+          <input type="password" placeholder="Password"/>
+          <button>Sign Up</button>
+        </form>
+      </div>
+
+      <div className="form-cont sign-in">
+        <form>
+            <h1>Sign In</h1>
+            <div className="social-icons">
+              <SignInButton />
+              <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+              <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+              <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+            </div>
+            <span>or use your email password</span>
+            <input type="email" placeholder="Email"/>
+            <input type="password" placeholder="Password"/>
+            <a href="#">Forget Your Password?</a>
+            <button>Sign In</button>
+          </form>
+        </div>
+
+        <div className="toggle-cont">
+          <div className="toggle">
+            <div className="toggle-panel toggle-left">
+              <h1>Welcome Back!</h1>
+              <p>Enter your personal details to use all of site features</p>
+              <button onClick={() => setHidden(!hidden)} className='hidden' id="login">Sign In</button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h1>Welcome, Friend!</h1>
+              <p>Enter your personal details to use all of site features</p>
+              <button onClick={() => setHidden(!hidden)} className='hidden' id="register">Sign Up</button>
+            </div>
+          </div>
+        </div>
+    </div>
   );
 }
 
